a244c2c2f86e94dc519b0c1483dcc66f33d37ca1
pie-mi <92503655+pie-mi@users.noreply.github.com>
2023-10-08 13:10:21 +0800

    create a GUI for the basic function
    the second edition work, but not the 1st one

---
+++ duplicate_with_GUI_2nd.py
@@ -0,0 +1,100 @@
+import pandas as pd  
+import tkinter as tk  
+from tkinter import filedialog  
+from tkinter import messagebox  
+import time  
+import os  
+  
+class Application(tk.Frame):  
+    def __init__(self, master=None):
+        master.title("去重小程序")  
+        super().__init__(master)  
+        self.master = master  
+        self.pack()  
+        self.create_widgets()  
+  
+    def create_widgets(self):
+        '''  
+        self.start_button = tk.Button(self)  
+        self.start_button["text"] = "选择文件和文件夹"  
+        self.start_button["command"] = self.ask_for_file_folder  
+        self.start_button.pack(side="top")  '''
+  
+        self.input_button = tk.Button(self)  
+        self.input_button["text"] = "选择输入文件"  
+        self.input_button["command"] = self.ask_for_input_file  
+        self.input_button.pack(side="top")  
+  
+        self.output_button = tk.Button(self)  
+        self.output_button["text"] = "选择输出文件夹"  
+        self.output_button["command"] = self.ask_for_output_folder  
+        self.output_button.pack(side="top")  
+  
+        self.start_button2 = tk.Button(self)  
+        self.start_button2["text"] = "开始"  
+        self.start_button2["command"] = self.process_data  
+        self.start_button2.pack(side="top")  
+  
+        self.text_box = tk.Text(self)  
+        self.text_box.pack(side="top", fill="both", expand=True)  
+  
+        self.text_box2 = tk.Text(self)  
+        self.text_box2.pack(side="bottom", fill="both", expand=True)  
+    '''
+    def ask_for_file_folder(self):  
+        filetypes = [("Excel Files", "*.xlsx *.xls"), ("All Files", "*.*")]  
+        filename = filedialog.askopenfilename(filetypes=filetypes)  
+        if filename:  
+            self.input_file = filename  
+            self.output_folder = filedialog.askdirectory()  
+            if self.input_file and self.output_folder:  
+                self.start_button2["state"] = "normal"  
+            else:  
+                messagebox.showinfo("错误", "请确保您已选择输入文件和输出文件夹")  
+    '''
+    def ask_for_input_file(self):  
+        filetypes = [("Excel Files", "*.xlsx *.xls"), ("All Files", "*.*")]  
+        filename = filedialog.askopenfilename(filetypes=filetypes)  
+        if filename:  
+            self.input_file = filename
+            self.text_box2.insert(tk.END, "需要处理的Excel文件路径： " + filename + "\n")  # 在文本框输出文件路径  
+            if self.output_folder:  
+                self.start_button2["state"] = "normal"  
+            else:  
+                messagebox.showinfo("错误", "请确保您已选择输出文件夹")  
+  
+    def ask_for_output_folder(self):  
+        foldername = filedialog.askdirectory()  
+        if foldername:  
+            self.output_folder = foldername
+            self.text_box2.insert(tk.END, "输出文件夹路径： " + foldername + "\n")  # 在文本框输出文件夹路径  
+            if self.input_file:  
+                self.start_button2["state"] = "normal"  
+            else:  
+                messagebox.showinfo("错误", "请确保您已选择输入文件")  
+  
+    def process_data(self):  
+        start_time = time.time()  
+        messagebox.showinfo("提示", "正在处理中...")  
+        df = pd.read_excel(self.input_file, sheet_name='基站承载视图1')  
+        duration = time.time() - start_time  
+        self.text_box.insert(tk.END, f"读取用时： {duration:.2f}秒\n")  
+        self.text_box.insert(tk.END, "正在处理中...\n")  
+        #start_time = time.time()  
+        df['设备端口'] = df['设备端口'].astype(str).apply(lambda x: x.split('.')[0] if '.' in x else x)
+        # 将两列合并为一列
+        df['new_column'] = df['设备名称'] + df['设备端口']
+        # 仅保留第一行
+        df = df.drop_duplicates(subset='new_column', keep='first')  
+        df.to_excel(os.path.join(self.output_folder, "output_" + time.strftime("%Y%m%d%H") + ".xlsx"))
+        duration = time.time() - start_time  
+        self.text_box.insert(tk.END, f"总耗时： {duration:.2f}秒\n")  
+        self.text_box.insert(tk.END, "处理完成\n")
+
+if __name__ == '__main__':  
+    root = tk.Tk()  
+    # 创建一个Label，显示作者信息  
+    author_label = tk.Label(root, text="作者：李泽钧 彭瑞安", font=("Arial", 8), anchor="w")  
+    author_label.pack(fill=tk.X, ipadx=5, pady=5)  # 使用padx和pady设置Label与其他组件的间距
+    app = Application(root)  
+    root.mainloop()
