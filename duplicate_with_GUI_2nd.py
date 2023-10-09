ca03fdb43be74a6fb6d79fb64a9c1287affd5b37
pie-mi <92503655+pie-mi@users.noreply.github.com>
2023-10-09 12:50:45 +0800

    NOT guarantee！add some 提示 and try another demand
    尝试加入了筛选union和down的代码，但不一定正确执行

--- duplicate_with_GUI_2nd.py
+++ duplicate_with_GUI_2nd.py
@@ -80,13 +80,21 @@ class Application(tk.Frame):
         duration = time.time() - start_time  
         self.text_box.insert(tk.END, f"读取用时： {duration:.2f}秒\n")  
         self.text_box.insert(tk.END, "正在处理中...\n")  
-        #start_time = time.time()  
+        #start_time = time.time()
+
+        # 删除"所有者"列中含有特定字符串的所在行  
+        #df = df[~df['基站厂家'].str.contains('联通')]
+        #df= df[(~df['基站厂家'].str.contains('联通', na=False)) | (df['基站厂家'].isna())] 
+        df = df[~df['基站厂家'].str.contains('联通', na=False)] 
+  
+        # 删除"状态"列为"down"的所在行  
+        df = df[df['端口状态'] != 'down']   
         df['设备端口'] = df['设备端口'].astype(str).apply(lambda x: x.split('.')[0] if '.' in x else x)
         # 将两列合并为一列
         df['new_column'] = df['设备名称'] + df['设备端口']
         # 仅保留第一行
         df = df.drop_duplicates(subset='new_column', keep='first')  
-        df.to_excel(os.path.join(self.output_folder, "output_" + time.strftime("%Y%m%d%H") + ".xlsx"))
+        df.to_excel(os.path.join(self.output_folder, "output_" + time.strftime("%Y%m%d%H%M") + ".xlsx"))
         duration = time.time() - start_time  
         self.text_box.insert(tk.END, f"总耗时： {duration:.2f}秒\n")  
         self.text_box.insert(tk.END, "处理完成\n")
@@ -96,5 +104,7 @@ if __name__ == '__main__':
     # 创建一个Label，显示作者信息  
     author_label = tk.Label(root, text="作者：李泽钧 彭瑞安", font=("Arial", 8), anchor="w")  
     author_label.pack(fill=tk.X, ipadx=5, pady=5)  # 使用padx和pady设置Label与其他组件的间距
+    author_label2 = tk.Label(root, text="请确保Excel表中含有名为“基站承载视图1”的sheet", font=("Arial", 12), anchor="center")  
+    author_label2.pack(fill=tk.X, ipadx=5, pady=5)  # 使用padx和pady设置Label与其他组件的间距
     app = Application(root)  
     root.mainloop()
